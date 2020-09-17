b04802c901a50acd8875d7f6c26c4de8dd2d6219
daniela2001-png <dmbmora@outlook.com>
2020-09-17 11:59:36 -0500

    add betty styles

--- monty.h
+++ monty.h
@@ -75,6 +75,7 @@ void sub_stack(stack_t **stack, unsigned int line_number);
 void mul_stack(stack_t **stack, unsigned int line_number);
 void div_stack(stack_t **stack, unsigned int line_number);
 void mod_stack(stack_t **stack, unsigned int line_number);
+void pstr_stack(stack_t **stack, unsigned int line_number);
 void pchar_stack(stack_t **stack, unsigned int line_number);
 void rotl_stack(stack_t **stack, unsigned int line_number);
 
